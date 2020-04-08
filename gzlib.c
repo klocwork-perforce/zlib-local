4d39634eaa531a585d47350f563944a55987f886
butylin <sergeibt@yandex.ru>
2020-04-08 10:44:53 -0400

    Update gzlib.c

--- gzlib.c
+++ gzlib.c
@@ -537,24 +537,16 @@ const char * ZEXPORT gzerror(file, errnum)
 
     /* get internal structure and check integrity */
     if (file == NULL)
-        return gnullcall();
+        return NULL;
     state = (gz_statep)file;
     if (state->mode != GZ_READ && state->mode != GZ_WRITE)
-        return gnullcall();
+        return NULL;
 
     /* return error information */
     if (errnum != NULL)
         *errnum = state->err;
     return state->err == Z_MEM_ERROR ? "out of memory" :
                                        (state->msg == NULL ? "" : state->msg);
-}
-
-const char * ZEXPORT gnullcall() {
-	return gnulltreturn();
-}
-
-const char * ZEXPORT gnulltreturn() {
-	return NULL;
 }
 
 /* -- see zlib.h -- */
